7122f99e1c73aa74aa784aa817c4088294c7f7ef
Osamah-Learn <osamah.amer@learnit.fi>
2021-09-22 16:03:39 +0300

    ScrollUp installed and invoked is done

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import { Route, Switch } from 'react-router';
 
+
 function App() {
   return (
     <>
--- my-project/src/components/Footer.js
+++ my-project/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 
 
@@ -23,12 +24,9 @@ function Footer() {
 
                         <div className="my-6 text-base text-color f-f-l">
                             <ul className="md:flex items-center">
-                                <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0">About</li>
-                                <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0">Features</li>
-                                <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0">Pricing</li>
-                                <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0">Careers</li>
-                                <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0">Help</li>
-                                <li className="cursor-pointer pt-4 lg:py-0">Privacy Policy</li>
+                                <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0"><Link to='/'>Home</Link></li>
+                                <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0"><Link to='/about'>About</Link></li>
+                                <li className="cursor-pointer pt-4 lg:py-0"><Link to='/contact'>Contact</Link></li>
                             </ul>
                         </div>
                         <div className="text-sm text-color mb-10 f-f-l">
