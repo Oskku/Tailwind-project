cad0ce1799ef8a5240c263ba7e871afa4a1abb1c
Osamah-Learn <osamah.amer@learnit.fi>
2021-09-23 16:35:19 +0300

    Phone Number adedd to contact form

--- my-project/src/components/Mail.js
+++ my-project/src/components/Mail.js
@@ -66,6 +66,14 @@ function Mail() {
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+              PhoneNumber
+            </label>
+            <input  register={register} required className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 " type="tel" id="phone" name="contact_number" placeholder="PuhelinNumero" />
+          </div>
+        </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
                 Your Message
